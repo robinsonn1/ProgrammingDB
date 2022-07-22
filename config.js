6761dd6ac78d2a254cf6208c6c907a7095fb5836
robinsonn1 <robinsonnavarro@gmail.com>
2022-07-22 17:42:51 -0500

    julio commit roB

--- config.js
+++ config.js
@@ -2,10 +2,10 @@ const env = process.env;
 
 const config = {
   db: { /* don't expose password or any sensitive info, done only for demo */
-    host: env.DB_HOST || 'freedb.tech',
-    user: env.DB_USER || 'freedbtech_robinson',
-    password: env.DB_PASSWORD || 'pass123',
-    database: env.DB_NAME || 'freedbtech_languagesrobin',
+    host: env.DB_HOST || 'sql.freedb.tech',
+    user: env.DB_USER || 'freedb_rnuserdatabase',
+    password: env.DB_PASSWORD || 'RWz8R5Pd6u!Dp9V',
+    database: env.DB_NAME || 'freedb_rndatabase',
   },
   listPerPage: env.LIST_PER_PAGE || 10,
 };
